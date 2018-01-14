028eefa2ca91e42509ff13f05722a9f57ddd50a0
Penguino <heriberto@melo.nyc>
2018-01-13 19:24:00 -0500

    More file modifications and confirming gems

--- lib/steam_deals_cli.rb
+++ lib/steam_deals_cli.rb
@@ -1,4 +1,11 @@
 require "steam_deals_cli/version"
+require "steam_deals_cli/steam_deals"
+require "steam_deals_cli/cli"
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 
 module SteamDealsCli
   # Your code goes here...
