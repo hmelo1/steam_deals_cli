a40665e21d78107d8ffe68ce986c094a517dbbd0
Penguino <heriberto@melo.nyc>
2018-01-14 10:44:00 -0500

    CLI configuration and testing

--- lib/steam_deals_cli/cli.rb
+++ lib/steam_deals_cli/cli.rb
@@ -1,4 +1,24 @@
 class SteamDealsCli::CLI
-  puts "HELLO!"
 
+  def call
+    puts "Welcome to Steam's daily and weekend deals!"
+    start
+  end
+
+  def start
+    puts "-----------------------------------------"
+    puts "\nWhich would you like to see? The Daily Deals or the Weekend Deals?"
+    puts "For Daily Deals press 1."
+    puts "For Weekend Deals press 2."
+
+    input = gets.strip
+
+    if input.to_i == 1
+      puts "Daily Deals:"
+    elsif input.to_i == 2
+      puts "Weekend Deals:"
+    else
+      puts "Invalid option; "
+    end
+  end
 end
